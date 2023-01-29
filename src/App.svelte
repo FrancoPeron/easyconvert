2aba1b052357e7375cbea0964151d717f97852a2
Franco Peron <francomatiasperon@gmail.com>
2023-01-28 22:20:16 -0300

    and a saber

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,52 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+// @ts-nocheck
+
+  import Dropzone from "svelte-file-dropzone";
+  let files = {
+    accepted: [],
+    rejected: []
+  };
+  function handleFilesSelect(e) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    files.accepted = [...files.accepted, ...acceptedFiles];
+    files.rejected = [...files.rejected, ...fileRejections];
+  }
+  function handleRemoveFile(e, index) {
+    files.accepted.splice(index, 1);
+    files.accepted = [...files.accepted];
+  }
+  function handleRemoveAll() {
+    files.accepted = [];
+  }
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  /* :global(.custom-dropzone) {
+  } */
+
+  .previewImg{
+    width: 400px;
   }
 </style>
+
+<Dropzone
+  on:drop={handleFilesSelect}
+  accept={['image/*']}
+  containerClasses="custom-dropzone">
+  <button>Choose images to upload</button>
+  <p>or</p>
+  <p>Drag and drop them here</p>
+</Dropzone>
+<div style="margin: 5px;">
+  {#if files.accepted.length > 0}
+    <button on:click={handleRemoveAll}>RemoveAll</button>
+  {/if}
+  {#each files.accepted as item, index}
+    <div>
+      <!-- <span>{item.name}</span> -->
+      <img class="previewImg" src={URL.createObjectURL(item)} alt="">
+      <button on:click={e => handleRemoveFile(e, index)}>Remove</button>
+    </div>
+  {/each}
+</div>
